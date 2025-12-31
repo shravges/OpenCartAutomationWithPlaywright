05896356ac043c71ab84951ab39a8ddb24e833ae
Shilpa S <shilpa.shravge@numito.com>
2025-12-31 16:34:12 +0000

    created config file for execution on docker container

---
+++ playwright.docker.config.ts
@@ -0,0 +1,104 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, './.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [['html'], ['list'],['allure-playwright'], ['playwright-html-reporter', { 
+      testFolder: 'tests',
+      title: 'Opencart HTML Report',
+      project: 'Opencart UI Tests',
+      release: '9.87.6',
+      testEnvironment: 'Test',
+      embedAssets: true,
+      embedAttachments: true,
+      outputFolder: 'playwright-html-report',
+      minifyAssets: true,
+      startServer: true,
+    }]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    baseURL: 'https://naveenautomationlabs.com',
+    video: 'on',
+    screenshot: 'on',
+    headless: true,
+
+    //connect to Docker playwright server
+    connectOptions: {
+wsEndpoint: 'ws://localhost:3000',
+    }
+  },
+
+  metadata: {
+      username: 'shilpa@gmail.com',
+      userpassword: 'test123'
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium-docker',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox-docker',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit-docker',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
